8065b44b6386b5b0b67c84310943f7226fed129d
Soyouse <141166090+Soyouse@users.noreply.github.com>
2024-02-07 16:55:24 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,12 @@
+
+<?php
+    include ("assets/utilities/header.php");
+?>
+    <div class="presentation position-absolute mt-5 top-25 bg-op text-white w-50 translate-middle start-50 p-3 d-flex flex-column align-items-center">
+        <h2 class="text-center">Notre Site</h2>
+        <p class="text-center">Bienvenue chez BlueChemistry, votre partenaire de confiance pour des solutions de santé accessibles et de qualité. Notre équipe dévouée travaille sans relâche pour vous offrir les meilleurs services dans le domaine de la vente de médicaments. Faisons connaissance avec ceux qui contribuent chaque jour à la réussite de notre mission.</p>
+        <a href="produits.php" class="btn btn-success w-50 " role="button">Nos produits</a>
+    </div>
+        </main> 
+    </body>
+</html>
