d3b016d8d34ba370b099d99e5bbc8459bc571697
Soyouse <141166090+Soyouse@users.noreply.github.com>
2024-02-12 09:22:15 +0100

    Delete functions directory

--- functions/login.php
+++
@@ -1,9 +0,0 @@
-<?php 
-try {
-    $conn = new PDO('mysql:host=localhost;dbname=medicament;charset=utf8', 'root', '' );
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Échec de la connexion : " . $e->getMessage();
-    die(); // Arrête l'exécution du script en cas d'échec de la connexion
-}
-?>
